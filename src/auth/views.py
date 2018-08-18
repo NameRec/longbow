004c8ca1594b4607197e9c0924b1100e650928df
Shtirliz <aka.NameRec@gmail.com>
2018-08-18 22:47:42 +0300

    Fixed #5 "Use created user".

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
@@ -7,10 +7,13 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = form.save()
+            # Fix #5 "Use created user". Strange, but it works... Without `authenticate` and access to raw password.
+            # Just call login after form save above.
+            #
+            # username = form.cleaned_data.get('username')
+            # raw_password = form.cleaned_data.get('password1')
+            # user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('index')
     else:
